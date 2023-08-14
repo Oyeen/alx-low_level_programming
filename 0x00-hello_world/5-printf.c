96227452a3e3b52f8fcd38e037f368f5156c9120
Oyeen <oyindamolaayoola5@gmail.com>
2023-08-14 13:21:11 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- * Return: Always return 0 (Success)
- *  */
+ * main - Entry of the program
+ * Return: Always returns (0)
+ *
+ */
 
 int main(void)
 	{
-		puts("\"Programming is like building a multilingual puzzle");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 		}
