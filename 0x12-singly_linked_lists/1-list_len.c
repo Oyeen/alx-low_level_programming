0c53d150a801f75be73cec4447e8230950849416
Oyeen <oyindamolaayoola5@gmail.com>
2023-09-21 15:55:06 -0700

    lists.h

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - check the code for Holberton School students.
+ * @h: represents the name of the list
+ * Return: represents the number of nodes in the list.
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
