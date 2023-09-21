c940b7e8911fae03957811927be4e7c58a70bf13
Oyeen <oyindamolaayoola5@gmail.com>
2023-09-21 15:59:00 -0700

    lists.h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,13 +3,12 @@
 
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * struct list_s - a singly linked list
+ * @str: string - 
+ * @len: represents the length of the string
+ * @next: the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct list_s
 {
