86bc22b62c3923f9930fd5aa044a2e66c34a8969
Oyeen <oyindamolaayoola5@gmail.com>
2023-09-26 03:44:49 -0700

    free

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * del_listint - will delete the head node of a linked list
- * @head: points to the first element of a linked list
+ * @head: first element of a linked list
  *
  * Return: returns the content of the data deleted
  * also returns 0 if the list is empty
