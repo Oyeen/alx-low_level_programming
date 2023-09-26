a981c3302e097dd2f6f6a3c9e56eb85cb5cae7bf
Oyeen <oyindamolaayoola5@gmail.com>
2023-09-26 02:19:21 -0700

    putchar

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,20 +1,23 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <string.h>
+
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
- * @next : a pointer to the next node
  * @n: an integer
- * Description: the node strcuture for singly linked list
+ * @next: a pointer to the next node
+ *
+ * Description: singly linked list node structure
+ *
  */
-
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
+
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
