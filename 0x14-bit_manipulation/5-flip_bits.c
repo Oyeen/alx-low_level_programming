6e3fd387e39494e705f16e5809c6c0d6befad152
Oyeen <oyindamolaayoola5@gmail.com>
2023-09-28 14:39:04 -0700

    flip floip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * flip_bits - flip to get from one number to another.
- * @n: the first number
- * @m: the second number
+ * @n: first number
+ * @m: second number
  * Return: the number of bits you would need to flip
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
