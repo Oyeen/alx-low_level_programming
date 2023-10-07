d16e9578805f90743a6bb757ca595708b746e10d
Oyeen <oyindamolaayoola5@gmail.com>
2023-10-06 19:55:01 -0700

    main.h file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-/* ELF header functions */
+/* The following contains functions of the ELF header */
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
