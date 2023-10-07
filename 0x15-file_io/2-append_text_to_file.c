f603012fe6434bbef657b95d84de9ce4fafedfcb
Oyeen <oyindamolaayoola5@gmail.com>
2023-10-06 19:37:03 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,41 +1,33 @@
 #include "main.h"
 
 /**
- * _strlen - the length of a string
- * @str: points to a string
- *
- * Return: string length; the length of the string
- */
-size_t _strlen(char *str)
-{
-	size_t i;
-
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
-}
-
-/**
- * append_text_to_file- attaches a text to a file, at the end.
- * @filename: filename
- * @text_content: the body of the file, what it contains
+ * append_text_to_file - attaches text to a file at the end
+ * @filename: the filename
+ * @text_content: body of file
  *
  * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;	
-	size_t len;
+	int fd, a, b = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
+
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
+	if (fd < 0)
 		return (-1);
-	if (text_content != NULL)
-		len = write(fd, text_content, _strlen(text_content));
+
+	if (text_content)
+	{
+		while (text_content[b])
+			b++;
+		a = write(fd, text_content, b);
+		if (a != b)
+			return (-1);
+	}
+
 	close(fd);
-	if (len == -1)
-		return (-1);
+
 	return (1);
 }
